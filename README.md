eb48f2e4783727931974ae3fe761f889273a9d45
53273_divya <divyatayde2015@gmail.com>
2022-08-01 16:14:36 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# exam
+# elearning
+This repository consist of node js application for transflower students
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const { response } = require('express');
-var express = require ('express')
-var app=express();
-app.get('/',(req,resp)=>{
-    resp.send("title");
 
+var express = require('express');
+var app = express();
+var path=require("path");
+var fs=require("fs");
+
+//server configuration
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(___dirname + '/index.html'));
 });
-app.listen(8080);
-console.log("server sarted at port 8080");
+ 
+
+app.get('/hello', function (req, res) {
+  console.log("CAlling rest api");
+  var person={firstName:'Ravi',lastName:'Tambade',age:47};
+  res.send(person);
+});
+
+ 
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at 8081");
+})
